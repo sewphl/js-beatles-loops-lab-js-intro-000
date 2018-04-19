d0c75f5610d34aa99cca42b4188c44a990fb6d6f
sharon <sharonoptsout@gmail.com>
2018-04-19 23:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function theBeatlesPlay(musicians,instruments) {
   var array = [];
-  for(i=1; i<musicians.length; i++){
+  for(i=0; i<musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i]);
   }
   return(array);
