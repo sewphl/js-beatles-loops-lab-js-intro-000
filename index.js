047cc970f39f7ca05335369ccb3d059ac428a109
sharon <sharonoptsout@gmail.com>
2018-04-19 23:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function theBeatlesPlay(musicians,instruments) {
-var array = [];
-for(i=0; i<musicians.length; i++){
-  array.push(musicians[i] + " plays " + instruments[i]);
-}
-return(array);
+  var array = [];
+  for(i=1; i<musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return(array);
 }
